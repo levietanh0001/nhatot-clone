62d8d38afd2f82a2cd6264f195d5690198f2d37d
Le William <lewilliam0001@gmail.com>
2023-06-14 21:37:31 +0700

    add responsive testing to check scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.scss";
 import Counter from "./components/counter/Counter";
 import Lorem from "./components/lorem/Lorem";
+import ResponsiveTesting from "./components/testing/ResponsiveTesting";
+
 
 function App() {
   return <div className="App">
+    <ResponsiveTesting />
     <div className="container">
       <Counter />
       <Lorem />
