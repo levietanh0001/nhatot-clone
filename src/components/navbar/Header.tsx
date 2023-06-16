107f65b6ed39f169e1523b025fbed4454c3326c6
Le William <lewilliam0001@gmail.com>
2023-06-16 23:35:15 +0700

    Create Header.tsx

---
+++ src/components/navbar/Header.tsx
@@ -0,0 +1,23 @@
+
+
+const Header = ({ children }) => {
+
+  return (<>
+    <header style={{
+      display: "flex",
+      flexFlow: "column nowrap",
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: "white",
+      // boxShadow: '0 0 1px 0'
+      outline: '1px solid red'
+    }}>
+      {children}
+    </header>
+    <div style={{ height: '100px' }}></div>
+  </>)
+}
+
+export default Header
