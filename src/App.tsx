609697dd2d01d5d194e812be4f5bace3c0e2c4a1
Le William <lewilliam0001@gmail.com>
2023-06-21 09:13:19 +0700

    add Body section

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
 import "./App.scss";
-import Counter from "./components/counter/Counter";
-import Lorem from "./components/lorem/Lorem";
-import ResponsiveTesting from "./components/testing/ResponsiveTesting";
-import ToggleThemeButton from './components/navbar/ToggleThemeButton';
 import TopNav from "./components/navbar/TopNav";
-import { useState, useEffect } from "react";
 import Header from "./layouts/Header";
+import Body from "./layouts/Body";
+import TopCarousel from "./components/carousel/TopCarousel";
+import KhamPhaNhaTot from "./components/kham-pha-nha-tot/KhamPhaNhaTot";
+import CongCuDichVu from "./components/cong-cu-dich-vu/CongCuDichVu";
+import MuaBanBDS from "./components/carousel/MuaBanBDS";
+import ChoThueBDS from "./components/carousel/ChoThueBDS";
+import DuAnBDS from "./components/carousel/DuAnBDS";
 
 
 function App() {
 
   return <div className="App">
-    <ToggleThemeButton />
-    <TopNav />
-    {/* <ResponsiveTesting /> */}
-    <div className='container'>
-      <Counter />
-      <Lorem />
-      
     <Header>
       <TopNav />
     </Header>
     <div className="header-bottom-spacer"></div>
+    <Body>
+      <TopCarousel />
+      <KhamPhaNhaTot />
+      <CongCuDichVu />
+      <MuaBanBDS />
+      <ChoThueBDS />
+      <DuAnBDS />
       <div className="content-spacer"></div>
-    </div>
-  </div>;
+    </Body>
+  </div>
 }
 
 export default App;
