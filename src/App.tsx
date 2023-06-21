db55e7fe6341bca2cef35bfe4873a026b365bf9e
Le William <lewilliam0001@gmail.com>
2023-06-21 09:12:07 +0700

    add Header section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ResponsiveTesting from "./components/testing/ResponsiveTesting";
 import ToggleThemeButton from './components/navbar/ToggleThemeButton';
 import TopNav from "./components/navbar/TopNav";
 import { useState, useEffect } from "react";
+import Header from "./layouts/Header";
 
 
 function App() {
@@ -17,6 +18,10 @@ function App() {
       <Counter />
       <Lorem />
       
+    <Header>
+      <TopNav />
+    </Header>
+    <div className="header-bottom-spacer"></div>
       <div className="content-spacer"></div>
     </div>
   </div>;
