2da59363e1bfeb02da25079d3410ff29a71a054d
Le William <lewilliam0001@gmail.com>
2023-06-26 16:13:35 +0700

    create breadcrumb

---
+++ src/components/breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,28 @@
+import { Breadcrumbs, Typography, Link } from '@mui/material';
+import styles from './Breadcrumb.module.scss';
+import { NavigateNext } from '@mui/icons-material';
+
+const Breadcrumb = () => {
+  return (
+    <div className={styles['wrapper']}>
+      <div className='container'>
+        <Breadcrumbs
+          aria-label='breadcrumb'
+          separator={<NavigateNext />}
+          maxItems={2}
+          itemsBeforeCollapse={2}
+          sx={{ fontSize: '0.875rem' }}
+        >
+          <Link underline='hover' href='#'>
+            Nhà tốt
+          </Link>
+          <Link underline='hover' href='#'>
+            Mua bán bất động sản
+          </Link>
+        </Breadcrumbs>
+      </div>
+    </div>
+  );
+};
+
+export default Breadcrumb;
