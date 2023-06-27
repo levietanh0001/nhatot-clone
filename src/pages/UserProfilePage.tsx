7c3d4bdee9bfe0cc7ae73aed6ea3fe1ce512fc7a
Le William <lewilliam0001@gmail.com>
2023-06-27 22:09:54 +0700

    .

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,7 +1,5 @@
 import AboutUs from "~/components/footer/about-us/AboutUs";
-import Breadcrumb from "~/components/breadcrumb/Breadcrumb";
 import TopNav from "~/components/navbar";
-import ContactUser from "~/components/product/ContactUser";
 import UserCard from "~/components/user-profile/UserCard";
 import UserProfile from "~/components/user-profile/UserProfile";
 import Footer from "~/layouts/Footer";
