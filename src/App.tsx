dd49d6538404e9ae5643865c3fdc65eff7a81060
Le William <lewilliam0001@gmail.com>
2023-06-27 15:35:44 +0700

    add Product and Chat pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import './App.scss';
 import HomePage from './pages/HomePage';
 import ProductListPage from './pages/ProductListPage';
 import PostPage from './pages/PostPage';
-import ProductDetailsPage from './pages/ProductDetailsPage';
+import ProductPage from './pages/ProductPage';
 import UserProfilePage from './pages/UserProfilePage';
+import ChatPage from './pages/ChatPage';
 
 function App() {
   return (
@@ -14,9 +15,10 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />}></Route>
           <Route path='/post' element={<PostPage />}></Route>
-          <Route path='/product' element={<ProductDetailsPage />}></Route>
+          <Route path='/product' element={<ProductPage />}></Route>
           <Route path='/product-list' element={<ProductListPage />}></Route>
           <Route path='/user-profile' element={<UserProfilePage />}></Route>
+          <Route path='/chat' element={<ChatPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
