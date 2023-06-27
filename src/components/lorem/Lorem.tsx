b3d045c4ee92d6eab168c3a7294a5ed71a3222af
Le William <lewilliam0001@gmail.com>
2023-06-27 22:08:48 +0700

    .

--- src/components/lorem/Lorem.tsx
+++ src/components/lorem/Lorem.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { less, more } from '~/features/lorem/loremSlice';
+import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from '~/app/store';
+import { less, more } from '~/features/lorem/loremSlice';
 
 const Lorem = () => {
   const currentLorem = useSelector((state: RootState) => state.lorem.value);
