fd1adf2a1f9a235e1ed2c6653a6d779e44a3b403
Le William <lewilliam0001@gmail.com>
2023-06-29 22:06:59 +0700

    setup firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,9 +15,28 @@ const firebaseConfig = {
   storageBucket: "nhatot-clone.appspot.com",
   messagingSenderId: "94473267843",
   appId: "1:94473267843:web:7a53c8c8516e79fd2dc9b6",
-  measurementId: "G-E7FMDY95B0"
+  measurementId: "G-E7FMDY95B0",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const auth = getAuth();
+
+const loginWithEmailAndPassword = (email, password, onSuccess, onError) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      onSuccess(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      
+      onError(errorCode, errorMessage);
+    });
+};
+
+export { auth, loginWithEmailAndPassword };
