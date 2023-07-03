e91b22ea01cdcba14e0939c1e25356b778abd4b9
Le William <lewilliam0001@gmail.com>
2023-07-03 10:13:03 +0700

    update toggle theme button

--- src/components/navbar/ToggleThemeButton.tsx
+++ src/components/navbar/ToggleThemeButton.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from "react";
-import LightModeIcon from "@mui/icons-material/LightMode";
-import DarkModeIcon from "@mui/icons-material/DarkMode";
-import { IconButton } from "@mui/material";
+import { useEffect, useState } from "react";
 import styles from "./ToggleThemeButton.module.scss";
-import clsx from "clsx";
+import { FaRegSun, FaRegMoon } from 'react-icons/fa';
 
 
 const ToggleThemeButton = () => {
-
-  const [darkTheme, setDarkTheme] = useState<boolean>(
-    () => localStorage.getItem("darkTheme") === 'true'? true: false
+  const [darkTheme, setDarkTheme] = useState<boolean>(() =>
+    localStorage.getItem("darkTheme") === "true" ? true : false
   );
 
   useEffect(() => {
@@ -25,11 +21,11 @@ const ToggleThemeButton = () => {
   useEffect(() => {
     if (darkTheme) {
       document.documentElement.classList.add("dark");
-      localStorage.setItem('darkTheme', 'true');
+      localStorage.setItem("darkTheme", "true");
       setDarkTheme(true);
     } else {
       document.documentElement.classList.remove("dark");
-      localStorage.setItem('darkTheme', 'false');
+      localStorage.setItem("darkTheme", "false");
       setDarkTheme(false);
     }
   }, [darkTheme]);
@@ -38,21 +34,38 @@ const ToggleThemeButton = () => {
     setDarkTheme(!darkTheme);
   };
 
-  const classes = clsx(styles["toggle-theme-btn"]);
-
   return (
-    <div>
-      <button
-        className={classes}
+    <>
+      <button 
+        className={styles["toggle-theme-btn"]}
         onClick={handleToggleTheme}
       >
-        {darkTheme ? (
-          <LightModeIcon sx={{ color: "white" }} />
-        ) : (
-          <DarkModeIcon sx={{ color: "black" }} />
-        )}
+        <span className={styles["icon-wrapper"]}>
+          {darkTheme ? (
+            <FaRegSun />
+          ) : (
+            <FaRegMoon />
+          )}
+        </span>
+        <span className="sr-only">Notifications</span>
       </button>
-    </div>
+
+      {/* <button
+        className={styles["toggle-theme-btn"]}
+        onClick={handleToggleTheme}
+        style={{
+          backgroundColor: "transparent",
+          border: 0,
+          padding: 0,
+        }}
+      >
+        {darkTheme ? (
+          <LightModeIcon sx={{ color: "white", border: 0 }} />
+        ) : (
+          <DarkModeIcon sx={{ color: "white", border: 0 }} />
+        )}
+      </button> */}
+    </>
   );
 };
 
