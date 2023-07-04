499f2e200b037f63f1655261e35d61921efd5616
Le William <lewilliam0001@gmail.com>
2023-07-04 08:34:08 +0700

    lazy load components on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,49 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.scss';
-import HomePage from './pages/HomePage';
-import ProductListPage from './pages/ProductListPage';
-import PostPage from './pages/PostPage';
-import ProductPage from './pages/ProductPage';
-import UserProfilePage from './pages/UserProfilePage';
-import ChatPage from './pages/ChatPage';
-import AdminPage from './pages/AdminPage';
-import Dashboard from './components/admin/dashboard/Dashboard';
-import LoginPage from './pages/LoginPage';
-import NotFound from './pages/NotFound';
+import "./App.scss";
+import HomePage from "./pages/HomePage";
+import ProductListPage from "./pages/ProductListPage";
+import PostPage from "./pages/PostPage";
+import ProductPage from "./pages/ProductPage";
+import UserProfilePage from "./pages/UserProfilePage";
+// import ChatPage from './pages/ChatPage';
+// import AdminPage from './pages/AdminPage';
+// import Dashboard from './components/admin/dashboard/Dashboard';
+// import LoginPage from './pages/LoginPage';
+// import NotFound from './pages/NotFound';
+
+const ChatPage = React.lazy(() => import("./pages/ChatPage"));
+const AdminPage = React.lazy(() => import("./pages/AdminPage"));
+const Dashboard = React.lazy(() => import("./components/admin/dashboard/Dashboard"));
+const LoginPage = React.lazy(() => import("./pages/LoginPage"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+
+export const LazyComponent = ({ Comp }) => {
+  return (
+    <React.Suspense fallback={<>...</>}>
+      {Comp}
+    </React.Suspense>
+  );
+};
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage />}></Route>
-          <Route path='/post' element={<PostPage />}></Route>
-          <Route path='/product' element={<ProductPage />}></Route>
-          <Route path='/product-list' element={<ProductListPage />}></Route>
-          <Route path='/user-profile' element={<UserProfilePage />}></Route>
-          <Route path='/chat' element={<ChatPage />}></Route>
-          <Route path='/login' element={<LoginPage />}></Route>
-          <Route path='/admin' element={<AdminPage />}>
-            <Route path='dashboard' element={<Dashboard />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/post" element={<PostPage />}></Route>
+          <Route path="/product" element={<ProductPage />}></Route>
+          <Route path="/product-list" element={<ProductListPage />}></Route>
+          <Route path="/user-profile" element={<UserProfilePage />}></Route>
+
+          <Route path="/chat" element={<LazyComponent Comp={<ChatPage />} />}></Route>
+          <Route path="/login" element={<LazyComponent Comp={<LoginPage />} />}></Route>
+          <Route path="/admin" element={<LazyComponent Comp={<AdminPage />} />}>
+            <Route path="dashboard" element={<LazyComponent Comp={<Dashboard />} />}></Route>
           </Route>
-          <Route path="*" element={<NotFound />}></Route>
+          <Route path="*" element={<LazyComponent Comp={<NotFound />} />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,15 +1,18 @@
-import AboutUs from "~/components/footer/about-us/AboutUs";
-import Glossary from "~/components/glossary/Glossary";
-import CongCuDichVu from "~/components/home/cong-cu-dich-vu/CongCuDichVu";
-import Keywords from "~/components/home/keywords/Keywords";
-import KhamPhaNhaTot from "~/components/home/kham-pha-nha-tot/KhamPhaNhaTot";
-import ChoThueBDSCardSlider from "~/components/home/product-card-slider/ChoThueBDSCardSlider";
-import DuAnBDSCardSlider from "~/components/home/product-card-slider/DuAnBDSCardSlider";
-import MuaBanBDSCardSlider from "~/components/home/product-card-slider/MuaBanBDSCardSlider";
-import TopCarousel from "~/components/home/top-carousel/TopCarousel";
-import TopNav from "~/components/navbar";
-import Footer from "~/layouts/Footer";
-import Header from "~/layouts/Header";
+import React from 'react';
+import { LazyComponent } from '~/App';
+import AboutUs from '~/components/footer/about-us/AboutUs';
+import CongCuDichVu from '~/components/home/cong-cu-dich-vu/CongCuDichVu';
+import Keywords from '~/components/home/keywords/Keywords';
+import KhamPhaNhaTot from '~/components/home/kham-pha-nha-tot/KhamPhaNhaTot';
+import ChoThueBDSCardSlider from '~/components/home/product-card-slider/ChoThueBDSCardSlider';
+import DuAnBDSCardSlider from '~/components/home/product-card-slider/DuAnBDSCardSlider';
+import MuaBanBDSCardSlider from '~/components/home/product-card-slider/MuaBanBDSCardSlider';
+import TopCarousel from '~/components/home/top-carousel/TopCarousel';
+import TopNav from '~/components/navbar';
+import Footer from '~/layouts/Footer';
+import Header from '~/layouts/Header';
+
+const Glossary = React.lazy(() => import('~/components/glossary/Glossary'));
 
 const HomePage = () => {
   return (
@@ -24,7 +27,7 @@ const HomePage = () => {
         <MuaBanBDSCardSlider />
         <ChoThueBDSCardSlider />
         <DuAnBDSCardSlider />
-        <Glossary />
+        <LazyComponent Comp={<Glossary />} />
         <Keywords />
       </body>
       <Footer>
