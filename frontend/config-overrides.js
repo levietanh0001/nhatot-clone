e67a749a68a2c2c56857253ddff1291dcbf94f8c
Le William <lewilliam0001@gmail.com>
2023-09-19 18:58:16 +0700

    bug fixes

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -3,13 +3,16 @@ const glob = require('glob');
 const path = require('path');
 
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
+// const PurgecssPlugin = require('purgecss-webpack-plugin');
 
 
 const addWebpackPlugins = (config, env) => {
   config.plugins.push(
     new UglifyJsPlugin({
-      uglifyOptions: { compress: { unused: true, dead_code: true } },
+      sourceMap: true,
+      uglifyOptions: {
+        compress: { unused: true, dead_code: true } 
+      },
     })
   );
   // config.plugins.push(new PurgecssPlugin({
@@ -19,26 +22,43 @@ const addWebpackPlugins = (config, env) => {
 };
 
 const disableChunking = (config) => {
-  config.optimization.splitChunks = {
-    cacheGroups: { default: false },
-  };
   config.optimization.runtimeChunk = false;
-
+  config.optimization.splitChunks = {
+    cacheGroups: {
+      default: false
+    }
+  };
   return config;
 };
 
-const cssLoader = adjustStyleLoaders(({ use }) => {
-  use.forEach((loader) => {
-    if (/mini-css-extract-plugin/.test(loader.loader)) {
-      loader.loader = require.resolve('style-loader');
-      loader.options = {};
-    }
-    loader.loader = require.resolve('sass-loader');
-    // loader.loader = require.resolve('css-loader');
-  });
-});
+const cssLoader = adjustStyleLoaders(({ use: [ , css, postcss, resolve, processor ] }) => {
+  css.options.sourceMap = true;         // css-loader
+  postcss.options.sourceMap = true;     // postcss-loader
+  // when enable pre-processor,
+  // resolve-url-loader will be enabled too
+  if (resolve) {
+    resolve.options.sourceMap = true;   // resolve-url-loader
+  }
+  // pre-processor
+  if (processor && processor.loader.includes('sass-loader')) {
+    processor.options.sourceMap = true; // sass-loader
+  }
+})
+
+// const cssLoader = adjustStyleLoaders(({ use }) => {
+//   use.forEach((loader) => {
+//     if (/mini-css-extract-plugin/.test(loader.loader)) {
+//       loader.loader = require.resolve('style-loader');
+//       loader.options = {};
+//     }
+//     // loader.loader = require.resolve('sass-loader');
+//     // loader.loader = require.resolve('css-loader');
+//   });
+// });
 
 const isProd = process.env.NODE_ENV.includes('prod');
+
+console.log({ isProd });
 
 // production
 module.exports = override(
@@ -49,5 +69,5 @@ module.exports = override(
   }),
   isProd ? addWebpackPlugins : null,
   isProd ? disableChunking : null,
-  isProd ? cssLoader : null
+  isProd ? cssLoader : null,
 );
