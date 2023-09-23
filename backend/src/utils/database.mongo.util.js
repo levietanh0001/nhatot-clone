5302225fe8114a3198b4b353b7681bb3ffa48156
Le Viet Anh <lewilliam0001@gmail.com>
2023-09-23 15:14:11 +0000

    Update database.mongo.util.js

--- backend/src/utils/database.mongo.util.js
+++ backend/src/utils/database.mongo.util.js
@@ -5,7 +5,10 @@ const connectToMongoDB = async () => {
   
   try {
 
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://root:123321@localhost:27017/nhatot?authSource=admin');
+    const conn = await mongoose.connect(
+      'mongodb://root:mongo_pass@$207.148.126.202:27017/nhatot?authSource=admin'
+      // process.env.MONGO_URI || 'mongodb://root:123321@localhost:27017/nhatot?authSource=admin'
+    );
 
     console.log('connected to mongodb');
 
