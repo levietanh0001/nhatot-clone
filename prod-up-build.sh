d00e590f4e18d2575c572a2596452971a37af760
Le William <lewilliam0001@gmail.com>
2023-09-25 17:26:29 +0700

    Update prod-up-build.sh

--- prod-up-build.sh
+++ prod-up-build.sh
@@ -1,6 +1,5 @@
 
-env $(cat ./backend/.env.prod && cat ./frontend/.env)
 docker volume create mysql_data
 docker volume create redis_data
 docker volume create mongo_data
-docker compose -f ./docker-compose.prod.yml up --build
+env $(cat ./backend/.env.prod && cat ./frontend/.env) docker compose -f ./docker-compose.prod.yml up --build
