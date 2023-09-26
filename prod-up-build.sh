fa88b6271bc6abebf6fea1c383291efcbb92c6c3
Le William <lewilliam0001@gmail.com>
2023-09-27 01:21:41 +0700

    create scripts folder

--- prod-up-build.sh
+++
@@ -1,5 +0,0 @@
-
-docker volume create mysql_data
-docker volume create redis_data
-docker volume create mongo_data
-env $(cat ./backend/.env.prod && cat ./frontend/.env) docker compose -f ./docker-compose.prod.yml up --build
