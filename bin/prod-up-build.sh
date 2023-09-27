9862f4b3e71edf40c8b6ba02a9d56e4adfe01631
Le William <lewilliam0001@gmail.com>
2023-09-27 21:49:06 +0700

    update production setup

--- bin/prod-up-build.sh
+++ bin/prod-up-build.sh
@@ -1,5 +1,4 @@
-
 docker volume create mysql_data
 docker volume create redis_data
 docker volume create mongo_data
-env $(cat ./backend/.env.prod && cat ./frontend/.env) docker compose -f ../docker-compose.prod.yml up --build
+env $(cat ./.env.prod) docker compose -f ./docker-compose.dev.yml up --build
--- bin/prod-up.sh
+++ bin/prod-up.sh
@@ -1,4 +1,4 @@
 docker volume create mysql_data
 docker volume create redis_data
 docker volume create mongo_data
-env $(cat ./.env.dev) docker compose -f ./docker-compose.dev.yml up --build
+env $(cat ./.env.prod) docker compose -f ./docker-compose.prod.yml up
